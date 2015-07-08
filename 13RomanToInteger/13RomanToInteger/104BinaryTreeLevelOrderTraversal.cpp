e8917badfccd1fddc7239f833a0586a98a242a51
wangmin <csustwangmin@163.com>
2015-07-08 10:59:26 +0800

    104AC

--- 13RomanToInteger/13RomanToInteger/104BinaryTreeLevelOrderTraversal.cpp
+++ 13RomanToInteger/13RomanToInteger/104BinaryTreeLevelOrderTraversal.cpp
@@ -11,49 +11,40 @@ public:
 		vector<vector<int>> result;
 		queue<TreeNode*> que;
 		vector<int> row;
-		bool serialNUll = false;
+		int count = 0;
+		int currentLeft = 1;
 
 		if (root == NULL)
 			return result;
 
 		que.push(root);
 
-
 		while (!que.empty())
 		{
 			TreeNode* temp2 = que.front();
 			que.pop();
-			
 
-			if (temp2 != NULL)
+			row.push_back(temp2->val);
+			currentLeft--;
+
+			if (temp2->left != NULL)
 			{
-				if (temp2->left != NULL)
-				{
-					que.push(temp2->left);
-				}
-				else
-				{
-					que.push(NULL);
-				}
-				if (temp2->right != NULL)
-				{
-					que.push(temp2->right);
-				}
-				else
-				{
-					que.push(NULL);
-				}
-				row.push_back(temp2->val);
-				serialNUll = false;
+				que.push(temp2->left);
+				count++;
+
 			}
-			else
+			if (temp2->right != NULL)
 			{
-				if (!serialNUll)
-				{
-					result.push_back(row);
-					row.clear();
-				}
-				serialNUll = true;
+				que.push(temp2->right);
+				count++;
+			}
+
+			if (currentLeft == 0)
+			{
+				result.push_back(row);
+				row.clear();
+				currentLeft = count;
+				count = 0;
 			}
 		}
 		return result;
--- 13RomanToInteger/13RomanToInteger/main.cpp
+++ 13RomanToInteger/13RomanToInteger/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <queue>
 
-#include "144BinaryTreePreoderTraveral.cpp"
+#include "104BinaryTreeLevelOrderTraversal.cpp"
 
 using namespace std;
 
@@ -12,45 +12,16 @@ using namespace std;
 
 int main()
 {
-	queue<int> que;
-	vector<int> row;
-	vector<vector<int>> result;
-	for (size_t i = 0; i < 10; i++)
-	{
-		que.push(i);
-		row.push_back(i);
-		result.push_back(row);
-		row.clear();
-	}
-
-	while (!que.empty())
-	{
-		cout << que.front() << endl;
-		que.pop();
-	}
-
-	for (size_t i = 0; i < result.size(); i++)
-	{
-		for (size_t j = 0; j < 1; j++)
-		{
-			cout << result[i][j] << endl;
-		}
-
-	}
-
 	
+	TreeNode root = TreeNode(1);
+	TreeNode right = TreeNode(2);
+	TreeNode left = TreeNode(3);
+	right.left = &left;
+	root.right = &right;
+	Solution mySolution = Solution();
+	vector<vector<int>> test = mySolution.levelOrder(&root);
 	
-	//TreeNode root = TreeNode(1);
-	//TreeNode right = TreeNode(2);
-	//TreeNode left = TreeNode(3);
-	//right.left = &left;
-	//root.right = &right;
-	//Solution mySolution = Solution();
-	//vector<int> test = mySolution.preorderTraversal(&root);
-	//for (size_t i = 0; i < test.size(); i++)
-	//{
-	//	cout << test[i] << endl;
-	//}
+	cout << test.size() << endl;
 
 	getchar();
 	getchar();

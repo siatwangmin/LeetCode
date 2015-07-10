c3188a67d11bd3c4c72f68e4d311b9bac0bc3414
wangmin <csustwangmin@163.com>
2015-07-10 16:16:39 +0800

    Problem : 70 Climbing Stairs State : AC Time : 0 ms

--- 13RomanToInteger/13RomanToInteger/70ClimbingStairs.cpp
+++ 13RomanToInteger/13RomanToInteger/70ClimbingStairs.cpp
@@ -11,30 +11,49 @@ using namespace std;
 
 
 //方法一：用递归的方法，Time Out
+//class Solution {
+//
+//private:
+//	int step1 = 1;
+//	int step2 = 2;
+//
+//	int DPSolve(int n)
+//	{
+//		if (n <= 0)
+//			return 0;
+//		if (n == 1)
+//		{
+//			return step1;
+//		}
+//		if (n == 2)
+//		{
+//			return step2;
+//		}
+//
+//		return DPSolve(n - 1) + DPSolve(n - 2);
+//
+//	}
+//public:
+//	int climbStairs(int n) {
+//		return DPSolve(n);
+//	}
+//};
+
+//方法二：用循环的方法
 class Solution {
-
-private:
-	int step1 = 1;
-	int step2 = 2;
-
-	int DPSolve(int n)
-	{
-		if (n <= 0)
-			return 0;
-		if (n == 1)
-		{
-			return step1;
-		}
-		if (n == 2)
-		{
-			return step2;
-		}
-
-		return DPSolve(n - 1) + DPSolve(n - 2);
-
-	}
 public:
 	int climbStairs(int n) {
-		return DPSolve(n);
+		vector<int> vec(n + 1, 0);
+		if (n <= 0)
+			return 0;
+		vec[1] = 1;
+		vec[2] = 2;
+
+		for (int i = 3; i < n + 1; i++)
+		{
+			vec[i] = vec[i - 1] + vec[i - 2];
+		}
+
+		return vec[n];
 	}
 };

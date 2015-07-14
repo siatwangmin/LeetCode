db439ed5898415ccb882f5dbfefc69a386f3d237
wangmin <csustwangmin@163.com>
2015-07-14 20:11:29 +0800

    99未完成

---
+++ 13RomanToInteger/13RomanToInteger/99RecoverBinarySearchTree.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include "tree.h"
+
+using namespace std;
+
+//*********************Description*********************************************************************
+//Two elements of a binary search tree (BST) are swapped by mistake.
+//
+//Recover the tree without changing its structure.
+//
+//Note:
+//A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+//*****************************************************************************************************
+
+class Solution {
+private:
+	stack<TreeNode*> sta;
+	TreeNode* cur;
+
+public:
+	void recoverTree(TreeNode* root) {
+
+	}
+};

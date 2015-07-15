91ee63ad866bac5ad36336608c03108399ab0928
wangmin <csustwangmin@163.com>
2015-07-15 15:41:39 +0800

    Problem : 232 Implement Queue using Stacks State : AC Time : 0

--- DataStructure/DataStructure/main.cpp
+++ DataStructure/DataStructure/main.cpp
@@ -3,11 +3,39 @@
 #include <vector>
 #include "tree.h"
 
+#include "232ImplementQueueusingStacks.cpp"
+
 
 using namespace std;
 
 int main()
 {
+	Queue myQue = Queue();
+	for (size_t i = 0; i < 10; i++)
+	{
+		myQue.push(i);
+	}
+
+	//queue<int> testQue;
+	//testQue.push(1);
+	//cout << testQue.front() << endl;
+	//testQue.pop();
+	//testQue.pop();
+	//cout << testQue.front() << endl;
+
+	//myQue.pop();
+
+	//while (!myQue.empty())
+	//{
+	//	cout << myQue.peek() << endl;
+	//	myQue.pop();
+	//}
+	for (size_t j = 0; j < 20; j++)
+	{
+		cout << j << ":" << myQue.peek() << endl;
+		myQue.pop();
+	}
+	
 	getchar();
 	getchar();
 	return 0 ;

fef531bd6cb8f0b964b82bb160cc308e8df7cb39
wangmin <csustwangmin@163.com>
2015-07-18 16:36:15 +0800

    Note : 有用时20ms的算法，改进 Problem : 136 Single Number State : AC Time : 68 ms

--- DataStructure/DataStructure/main.cpp
+++ DataStructure/DataStructure/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include "link.h"
 
-#include "42TrappingRainWater.cpp"
+#include "136SingleNumber.cpp"
 
 
 using namespace std;
@@ -13,26 +13,20 @@ using namespace std;
 int main()
 {
 	vector<int> test;
-	test.push_back(0);
-	test.push_back(1);
-	test.push_back(0);
-	test.push_back(2);
-	test.push_back(1);
-	test.push_back(0);
-	test.push_back(1);
-	test.push_back(3);
-	test.push_back(2);
-	test.push_back(1);
-	test.push_back(2);
-	test.push_back(1);
+	//test.push_back(0);
+	//test.push_back(0);
+	//test.push_back(1);
+	//test.push_back(2);
+	//test.push_back(2);
+	//test.push_back(3);
+	//test.push_back(3);
+	//test.push_back(4);
+	//test.push_back(4);
+	//test.push_back(5);
+	//test.push_back(5);
 
-	vector<int> test2;
-	test2.push_back(4);
-	test2.push_back(2);
-	test2.push_back(3);
 	Solution mySolution = Solution();
-	int re = mySolution.trap(test);
-	int re2 = mySolution.trap(test2);
+	int re = mySolution.singleNumber(test);
 
 	getchar();
 	getchar();
